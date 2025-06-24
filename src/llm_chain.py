8e3766628e08d2d63e2ed52ac3268245d37cc22f
toanlk <toanlk@bookshare.com>
2025-06-24 15:01:57 +0700

    feat(*): run some test langchain

---
+++ src/llm_chain.py
@@ -0,0 +1,24 @@
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain.chat_models import init_chat_model
+from langchain_core.prompts import ChatPromptTemplate
+
+model = init_chat_model("gpt-4o-mini", model_provider="openai")
+
+messages = [
+    SystemMessage("Translate the following from English into Italian"),
+    HumanMessage("hi!"),
+]
+
+model.invoke(messages)
+
+for token in model.stream(messages):
+    print(token.content, end="|")
+
+system_template = "Translate the following from English into {language}"
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("user", "{text}")]
+)
+prompt = prompt_template.invoke({"language": "Italian", "text": "hi!"})
+
+response = model.invoke(prompt)
+print(response.content)
